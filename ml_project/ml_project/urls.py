e7049566ca3688f5077712495ac24b97616cc966
dishita108 <dishita108@gmail.com>
2021-12-04 19:07:56 +0530

    updated code for music recommend

--- ml_project/ml_project/urls.py
+++ ml_project/ml_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ',include ('emotion_recog.urls'))
+    path('',include ('userauth.urls'))
 ]
